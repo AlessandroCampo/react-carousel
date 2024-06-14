4d316662362ba25a65ff535e04816955e5284d25
Alessandro Campo <alessandrocampo97@gmail.com>
2024-06-14 12:41:55 +0200

    feat: assets loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import pokemonArray from "./Data";
+
 const app = () => {
   return (
     <>
-      <h2>
-        App
-      </h2>
+      {
+        pokemonArray.map(p => {
+          return <img src={p.Image}></img>
+        })
+      }
     </>
   )
 }
---
+++ src/Data.jsx
@@ -0,0 +1,54 @@
+import Charizard from './assets/Charizard.jpg'
+import Gardevoir from './assets/Gardevoir.jpg'
+import Gengar from './assets/Gengar.jpg'
+import Giratina from './assets/Giratina.jpg'
+import Lugia from './assets/Lugia.jpg'
+import Moltres from './assets/Moltres.jpg'
+import Rayquaza from './assets/Rayquaza.jpg'
+import Tyranitar from './assets/Tyranitar.jpg'
+
+
+const pokemonArray = [
+    {
+        Name: 'Charizard',
+        description: 'Charizard is a fire/flying type Pokémon known for its powerful flame.',
+        Image: Charizard,
+    },
+    {
+        Name: 'Gardevoir',
+        description: 'Gardevoir is a psychic/fairy type Pokémon that can sense the emotions of others.',
+        Image: Gardevoir,
+    },
+    {
+        Name: 'Gengar',
+        description: 'Gengar is a ghost/poison type Pokémon that can hide in the shadows.',
+        Image: Gengar,
+    },
+    {
+        Name: 'Giratina',
+        description: 'Giratina is a ghost/dragon type Pokémon that lives in the Distortion World.',
+        Image: Giratina,
+    },
+    {
+        Name: 'Lugia',
+        description: 'Lugia is a psychic/flying type Pokémon, often considered the guardian of the seas.',
+        Image: Lugia,
+    },
+    {
+        Name: 'Moltres',
+        description: 'Moltres is a fire/flying type legendary Pokémon known as the embodiment of fire.',
+        Image: Moltres,
+    },
+    {
+        Name: 'Rayquaza',
+        description: 'Rayquaza is a dragon/flying type legendary Pokémon that soars in the ozone layer.',
+        Image: Rayquaza,
+    },
+    {
+        Name: 'Tyranitar',
+        description: 'Tyranitar is a rock/dark type Pokémon known for its incredible strength and resilience.',
+        Image: Tyranitar,
+    },
+];
+
+export default pokemonArray;
